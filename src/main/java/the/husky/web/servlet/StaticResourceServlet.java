278e92429d683ba98a15a44efdaba3abc57596b2
GuchiTheHasky <guchithehasky@gmail.com>
2023-07-08 20:37:22 +0300

    added NIO

--- src/main/java/the/husky/web/servlet/StaticResourceServlet.java
+++ src/main/java/the/husky/web/servlet/StaticResourceServlet.java
@@ -8,6 +8,10 @@ import lombok.Cleanup;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 
 public class StaticResourceServlet extends HttpServlet {
     @Override
@@ -34,12 +38,24 @@ public class StaticResourceServlet extends HttpServlet {
         }
     }
 
-    private void streamResourceContent(HttpServletResponse response, InputStream inputStream)
-            throws IOException {
-        byte[] buffer = new byte[4096];
-        int bytes;
-        while ((bytes = inputStream.read(buffer)) != -1) {
-            response.getOutputStream().write(buffer, 0, bytes);
+    private void streamResourceContent(HttpServletResponse response, InputStream inputStream) throws IOException {
+        try (ReadableByteChannel inputChannel = Channels.newChannel(inputStream);
+             WritableByteChannel outputChannel = Channels.newChannel(response.getOutputStream())) {
+
+            int bufferSize = 4096;
+            int growFactor = 2;
+            ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+            int bytesRead;
+            while ((bytesRead = inputChannel.read(buffer)) != -1) {
+                buffer.flip();
+                outputChannel.write(buffer);
+                buffer.clear();
+
+                if (buffer.remaining() < bytesRead) {
+                    bufferSize *= growFactor;
+                    buffer = ByteBuffer.allocate(bufferSize);
+                }
+            }
         }
     }
 }
