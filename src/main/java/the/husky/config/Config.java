72a78a41be06ee04a1571d3f29086476ef11292f
GuchiTheHasky <guchithehasky@gmail.com>
2023-08-21 13:57:17 +0300

    added RELEASE in pom.xml; delete freemarkerConfigurer

--- src/main/java/the/husky/config/Config.java
+++ src/main/java/the/husky/config/Config.java
@@ -3,19 +3,11 @@ package the.husky.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "the.husky")
 public class Config {
-
-    @Bean
-    public FreeMarkerConfigurer freeMarkerConfigurer() {
-        FreeMarkerConfigurer freemarkerConfig = new FreeMarkerConfigurer();
-        freemarkerConfig.setTemplateLoaderPath("classpath:/templates/");
-        return freemarkerConfig;
-    }
 
     @Bean
     public FreeMarkerViewResolver freeMarkerViewResolver() {
