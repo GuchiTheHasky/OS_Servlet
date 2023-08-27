e6fa781e406a0f5d09f42aa8a54d3f556b046f94
GuchiTheHasky <guchithehasky@gmail.com>
2023-08-27 13:42:56 +0300

    update config.java

--- src/main/java/the/husky/config/Config.java
+++ src/main/java/the/husky/config/Config.java
@@ -1,9 +1,11 @@
 package the.husky.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+import the.husky.filter.WebFilter;
 
 @Configuration
 @ComponentScan(basePackages = "the.husky")
